aaf93b1f8f01b6c382eefb39176780b0d749358b
candescent-flow-1570 <f.iqbal.w@gmail.com>
2018-10-25 17:13:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
+var n = a
+function inc(n) {
+  n ++
+  return n
+}
+function dec(n) {
+  n --
+  return n
+}
+function makeInt(n) {
+  return parseInt(n)
+}
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+function makeInt(sldkjflksjf) {
+  return parseInt(sldkjflksjf)
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
+function preserveDecimal(sldkjflksjf) {
+  return parseFloat(sldkjflksjf)
+}
